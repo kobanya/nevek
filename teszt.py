e813775faac1d5e5344c5665663934e10070e13d
bela <nagy.bela.budapest@gmail.com>
2023-05-15 00:08:26 +0200

    Kép hozzáadása

--- teszt.py
+++ teszt.py
@@ -1,19 +1,11 @@
+
 import magyar
 import random
 
+telepulesek = random.sample(magyar.telepules, k=80)  # 55 nem ismétlődő magyar település listája
+tordelt = magyar.tordel(telepulesek, 8)              # soronként 8 elemmel azonnal kiírva a terminálra
 
-utca = random.sample(magyar.utca, k=5)                          # véletlenszerűen kiválasztott 5 utca
-telepulesek = random.choices(magyar.telepules, k=5)             # véletlenül kiválasztott 5 magyar település neve
-ferfi_keresztnev =random.choices(magyar.keresztnev_f, k=7)      # véletlenül kiválasztott 7 magyarférfi keresztnév
-vezeteknevek= random.choices(magyar.vezeteknev, k=8)            # véletlenül kiválasztott 8 magyar vezetéknév
+# a tördelt listát azonnal kiírja, nincs szükség PRINT parancsra
 
-print('\n\nListát ad vissza , megadott elemszámmal :\n')
-print(telepulesek)
-print(utca)
-print(ferfi_keresztnev)
-print(vezeteknevek)
 
-print()
-print()
-print()
 
