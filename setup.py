788f3e9a05d22b98f451493957bcda2ca357d9c2
kobanya <nagy.bela.budapest@gmail.com>
2024-05-26 00:34:15 +0200

    jav

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setup(
     name="magyar",
-    version="4.0.2",
+    version="4.0.4",
     author="Nagy Béla",
     author_email="nagy.belabudapest@gmail.com",
-    description="Hungarian lists of names,animals,foods, fruits, rivers ,GPS ..",
+    description="Hungarian lists of names,animals,foods, fruits, rivers ,GPS .. ZIP",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kobanya/nevek",
